c74c65e58004aeac414212027297ee8ddbcc3de9
sameer223 <78470082+sameer223@users.noreply.github.com>
2021-02-03 23:19:30 +0530

    Update sendmail.js

--- routes/sendmail.js
+++ routes/sendmail.js
@@ -2,17 +2,21 @@
 var express = require('express');
 var router = express.Router();
 const nodemailer = require('nodemailer');
-let transporter = nodemailer.createTransport({
-   service: "Office365",
-            host: "smtp.office365.com",  
-            secureConnection: false,
-            port: 25,
-    auth: {
-        user: 'sells@nityamedhall.com',
-        pass: 'Raaj@123456789'
-    },
-    tls: { rejectUnauthorized: false }
-});
+var smtpTransport = require("nodemailer-smtp-transport");
+
+let transporter = nodemailer.createTransport(smtpTransport({
+      service: 'Godaddy',
+      // host: 'smtpout.asia.secureserver.net',
+      secureConnection: false,
+      port: 25,
+
+     auth: {
+         user: 'sells@nityamedhall.com',
+         pass: 'sandeep@0288'
+     },
+          // tls: {rejectUnauthorized: false},
+          // debug:true
+   }));
 transporter.verify(function (error, success) {
     if (error) {
         console.log(error);
@@ -32,46 +36,48 @@ router.get('/', function (req, res, next) {
                         </body>\n\
                         </html>";
     var sendmailrevemail = "<html>\n\
-                        <body>\n\
-                        <h1>Thank you for your query</h1>\n\
-                        <h6>We attached your query details details</h6>\n\
-                        <p> Name: " +  req.param('name') + "</p>\n\
-                        <p> Email: " +  req.param('email') + "</p>\n\
-                        <p> Query: " +  req.param('msg') + "</p>\n\
-                        <p> Newsletter: " +  req.param('newsletter') + "</p>\n\
-                       </body>\n\
+                        <body style='height:100%'>\n\
+                        <h3>Thank you " +  req.param('name') + " for your query on Nitya Med Hall</h3>\n\
+                        <h5>You can now login with follwing Credential :- </h5>\n\
+                        <p> Email: <b>" +  req.param('email') + "</b></p>\n\
+                        <p> Password: <b>" +  req.param('pass') + "</b></p>\n\
+                        <p> Click on the below link to login:</p>\n\
+                        <a href='https://nityamedhall.com/login/index.html'>LOGIN NOW </a>\n\
+                        </body>\n\
                        </html>";
 
     var sender_email = req.param('email');
 
     let mailOptions = {
-        from: 'shinigami121999@gmail.com',
-        to: 'ranjitkumar448@yahoo.com',
-        subject: "a new query arrived from " +  req.param('email') + "",
-        html: sendmailreq
-    };
-
-    let mailreverse = {
-        from: 'shinigami121999@gmail.com',
+        from: 'sells@nityamedhall.com',
         to: sender_email,
-        subject: "your query on domain.com",
+        subject: "Your query on Nitya Med Hall",
         html: sendmailrevemail
     };
+
+    // let mailreverse = {
+    //     from: 'sells@nityamedhall.com',
+    //     to: sender_email,
+    //     subject: "Your query on Nitya Med Hall",
+    //     html: sendmailrevemail
+    // };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
+          res.send(info);
             return console.log(error);
         }
+        res.send(info);
+
         console.log('Message %s sent: %s', info.messageId, info.response);
     });
 
 
-    transporter.sendMail(mailreverse, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-    });
-    res.send("email send");
+    // transporter.sendMail(mailreverse, (error, info) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     console.log('Message %s sent: %s', info.messageId, info.response);
+    // });
 
 });
 
