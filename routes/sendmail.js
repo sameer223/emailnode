727bb44e127db81ed2ce8803adca698895a469f9
sameer223 <78470082+sameer223@users.noreply.github.com>
2021-02-03 14:28:42 +0530

    Update sendmail.js

--- routes/sendmail.js
+++ routes/sendmail.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
-    host: 'gmail.com',
+    host: 'gmail',
     port: 25,
     secure: false,
     auth: {
